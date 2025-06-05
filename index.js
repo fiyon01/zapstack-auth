700dd5eab1833ceca7356884f858fd2f5d5948e6
fiyon01 <147448591+fiyon01@users.noreply.github.com>
2025-06-06 02:42:35 +0300

    changes

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
-const {jwtDecode} = require("jwt-decode");
+const { jwtDecode } = require("jwt-decode");
 
-const ZapAuth = {
+class ZapAuth {
+  constructor() {
+    this.backendUrl = "";
+    this.redirectUrl = "";
+    this.zapKey = "";
+  }
+
   init({ backendUrl, redirectUrl, zapKey }) {
     this.backendUrl = backendUrl;
     this.redirectUrl = redirectUrl;
     this.zapKey = zapKey;
-  },
+  }
+
   loginWithGoogle() {
     window.location.href = `${this.backendUrl}/auth/google?zap_key=${this.zapKey}`;
-  },
+  }
+
   loginWithGithub() {
     window.location.href = `${this.backendUrl}/auth/github?zap_key=${this.zapKey}`;
-  },
+  }
+
   decodeToken(token) {
     return jwtDecode(token);
-  },
-};
+  }
+}
 
-module.exports = ZapAuth;
+module.exports = new ZapAuth(); // export as singleton
